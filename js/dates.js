d58e47e98217cf025a1b50fb46556825a21e19b2
Jacob Terrell <jacobit97@hotmail.com>
2021-10-03 23:47:40 -0600

    Whoops this was supposed to be added

--- js/dates.js
+++ js/dates.js
@@ -1,3 +1,3 @@
 document.getElementById('copyrightyear').textContent = new Date().getFullYear();
 // document.getElementById('last-updated').textContent = `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()} ${new Date().toLocaleTimeString()}`;
-document.getElementById('last-updated').textContent = `${new Date(document.lastModified).getMonth() + 1}/${new Date(document.lastModified).getDate()}/${new Date(document.lastModified).getFullYear()} ${new Date(document.lastModified).toLocaleTimeString()}`;
+document.getElementById('last-updated').textContent = `${new Date(document.lastModified).getMonth() + 1}/${new Date(document.lastModified).getDate() - 1}/${new Date(document.lastModified).getFullYear()} ${new Date(document.lastModified).toLocaleTimeString()}`;
