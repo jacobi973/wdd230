3b0ac14996efbb74332b7279c7d82d96c8680a29
Jacob Terrell <jacobit97@hotmail.com>
2021-11-12 21:39:22 -0700

    removed

--- lesson8/js/scripts.js
+++
@@ -1,8 +0,0 @@
-let date = new Date(document.lastModified);
-let time = date.toLocaleString("en-US");
-
-document.getElementById("lastTime").textContent = time;
-
-function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("hide");
-}
