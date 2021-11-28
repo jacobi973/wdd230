afb72870ff033823438919e8f665ca13c15094a2
Jacob Terrell <jacobit97@hotmail.com>
2021-11-27 17:27:55 -0700

    lesson-7 activity

---
+++ lesson7/js/script.js
@@ -0,0 +1,31 @@
+const images = document.querySelectorAll("[data-src]")
+
+function preLoadImage(img) {
+    const src = img.getAttribute("data-src")
+    if (!src) {
+        return;
+    }
+
+    img.src = src;
+};
+
+
+const options = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+}
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preLoadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    });
+}, options);
+
+images.forEach(image => {
+    imgObserver.observe(image)
+});
