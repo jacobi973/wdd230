8a168eb923b77e55d0d94e72911b8a78a9717262
Jacob Terrell <jacobit97@hotmail.com>
2021-11-29 20:44:27 -0700

    prophet-section added

---
+++ lesson9/js/prophet.js
@@ -0,0 +1,26 @@
+getProphets();
+async function getProphets() {
+  const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json'
+  const responseJSON = await (await fetch(requestURL)).json();
+
+  const prophets = responseJSON['prophets']
+  prophets.forEach(element => {
+    let card = document.createElement('section')
+    let h2 = document.createElement('h2')
+    let span1 = document.createElement('span')
+    let span2 = document.createElement('span')
+    let image = document.createElement('img')
+
+    h2.textContent = element.name + ' ' + element.lastname
+    span1.textContent = 'Date of birth: ' + element.birthdate
+    span2.textContent = 'Place of birth: ' + element.birthplace
+    image.setAttribute('src', element.imageurl)
+
+    card.appendChild(h2)
+    card.appendChild(span1)
+    card.appendChild(span2)
+    card.appendChild(image)
+
+    document.querySelector('div.cards').appendChild(card)
+  })
+}
